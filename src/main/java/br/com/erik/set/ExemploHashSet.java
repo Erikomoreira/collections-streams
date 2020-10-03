bdb1a7f9bb240a4ce6745c9d940cde2b2afed9a5
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-10-03 00:55:17 -0300

    map

--- src/main/java/br/com/erik/set/ExemploHashSet.java
+++ src/main/java/br/com/erik/set/ExemploHashSet.java
@@ -1,4 +1,50 @@
 package br.com.erik.set;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class ExemploHashSet {
+
+    public static void main(String[] args) {
+
+        Set<Double> notasAlunos = new HashSet<>();
+
+        //adiciona as notas no set
+        notasAlunos.add(5.8);
+        notasAlunos.add(9.3);
+        notasAlunos.add(6.5);
+        notasAlunos.add(10.0);
+        notasAlunos.add(5.4);
+        notasAlunos.add(7.3);
+        notasAlunos.add(3.8);
+        notasAlunos.add(4.0);
+
+        System.out.println(notasAlunos);
+
+        //Remove a nota do Set
+        notasAlunos.remove(3.8);
+
+        //Retorna a quantidade de itens do Set
+        System.out.println(notasAlunos.size());
+
+        //Navega em todos os itens do iterator
+        Iterator<Double> iterator = notasAlunos.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        for(Double nota: notasAlunos){
+            System.out.println(nota);
+        }
+
+        notasAlunos.clear();
+
+        //Retorna se o set está vazio ou não
+        System.out.println(notasAlunos.isEmpty());
+
+    }
+
+
 }
--- src/main/java/br/com/erik/set/ExemploTreeSet.java
+++ src/main/java/br/com/erik/set/ExemploTreeSet.java
@@ -1,4 +1,45 @@
 package br.com.erik.set;
 
+import java.util.TreeSet;
+
 public class ExemploTreeSet {
+
+    public static void main(String[] args) {
+
+        TreeSet<String> treeCapitais = new TreeSet<>();
+
+        treeCapitais.add("Porto Alegre");
+        treeCapitais.add("Florianópolis");
+        treeCapitais.add("Curitiba");
+        treeCapitais.add("São Paulo");
+        treeCapitais.add("Rio de Janeiro");
+        treeCapitais.add("Belo Horizonte");
+
+        System.out.println(treeCapitais);
+
+        //Retorna a primeira capital no topo da árvore
+        System.out.println(treeCapitais.first());
+
+        //Retorna a última capital no final da árvore
+        System.out.println(treeCapitais.last());
+
+        //Retorna a primeira capital abaixo na árvore da capital parametrizada
+        System.out.println(treeCapitais.lower("Florianópolis"));
+
+        //Retorna a primeira capital acima na árvore da capital parametrizada
+        System.out.println(treeCapitais.higher("Florianópolis"));
+
+        //Exibe todas as capitais no console
+        System.out.println(treeCapitais);
+
+        //Retorna a primeira capital no topo da árvore, removendo do set
+        System.out.println(treeCapitais.pollFirst());
+
+        //Retorna a primeira capital no final da árvore, removendo do set
+        System.out.println(treeCapitais.pollLast());
+
+        //Exibe todas as capitas no console.
+        System.out.println(treeCapitais);
+
+    }
 }
